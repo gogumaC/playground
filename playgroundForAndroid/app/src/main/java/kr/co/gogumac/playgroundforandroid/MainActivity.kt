7dd7145fe7ddba11f62ee030c218d92d280fbdc8
gogumaC(mac mini) <kimu2371@naver.com>
2024-07-29 15:19:01 +0900

    3. Make runtime permission request workflow

--- playgroundForAndroid/app/src/main/java/kr/co/gogumac/playgroundforandroid/MainActivity.kt
+++ playgroundForAndroid/app/src/main/java/kr/co/gogumac/playgroundforandroid/MainActivity.kt
@@ -1,9 +1,11 @@
 package kr.co.gogumac.playgroundforandroid
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import kr.co.gogumac.playgroundforandroid.ui.theme.PlaygroundForAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +31,7 @@ class MainActivity : ComponentActivity() {
             }
         }
         setContent {
+            requestPermission(requestPermissionLauncher)
             PlaygroundForAndroidTheme {
 
                 // A surface container using the 'background' color from the theme
@@ -40,7 +44,24 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun requestPermission(launcher: ActivityResultLauncher<String>){
+        when{
+            ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED->{
+                Toast.makeText(this,"권한이 이미 승인됨",Toast.LENGTH_SHORT).show()
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)->{
+                Toast.makeText(this,"권한이 필요한 이유를 설명",Toast.LENGTH_SHORT).show()
+            }
+            else->{
+                Toast.makeText(this,"권한 요청",Toast.LENGTH_SHORT).show()
+                launcher.launch(android.Manifest.permission.CAMERA)
+            }
+        }
+    }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
