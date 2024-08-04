db600d2bd9fd5bf7018f1eac4b98261c23fc47f7
gogumaC(mac mini) <kimu2371@naver.com>
2024-08-04 23:11:40 +0900

    4. Create navHost and Navigate screen

--- playgroundForAndroid/app/src/main/java/kr/co/gogumac/playgroundforandroid/MainActivity.kt
+++ playgroundForAndroid/app/src/main/java/kr/co/gogumac/playgroundforandroid/MainActivity.kt
@@ -3,11 +3,20 @@ package kr.co.gogumac.playgroundforandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import kotlinx.serialization.Serializable
 import kr.co.gogumac.playgroundforandroid.ui.theme.PlaygroundForAndroidTheme
 
@@ -16,7 +25,7 @@ import kr.co.gogumac.playgroundforandroid.ui.theme.PlaygroundForAndroidTheme
 object ScreenA
 
 @Serializable
-data class ScreenB(val arg:String)
+data class ScreenB(val arg: String)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,10 +37,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController= rememberNavController()
+                    val navController = rememberNavController()
 
+                    NavHost(navController = navController, startDestination = ScreenA) {
+                        composable<ScreenA> {
+                            ScreenA(navigateToScreenB = { navController.navigate(ScreenB("Hello")) })
+                        }
+                        composable<ScreenB> { navBackStackEntry ->
+                            val route = navBackStackEntry.toRoute<ScreenB>()
+                            ScreenB(
+                                navigateToScreenA = { navController.navigate(ScreenA) },
+                                arg = route.arg
+                            )
+                        }
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ScreenA(navigateToScreenB: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text("ScreenA")
+        Button(onClick = navigateToScreenB) {
+            Text("go to ScreenB")
+        }
+    }
+}
+
+@Composable
+fun ScreenB(navigateToScreenA: () -> Unit, arg: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text("ScreenB : arg = $arg")
+        Button(onClick = navigateToScreenA) {
+            Text("go to ScreenA")
         }
     }
 }
