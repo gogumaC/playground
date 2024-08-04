474a114675b8fd60d5d212210bcf80fc1a36c403
gogumaC(mac mini) <kimu2371@naver.com>
2024-08-04 20:53:05 +0900

    4. Create route

--- playgroundForAndroid/app/src/main/java/com/gogumac/playgroundforandroid/MainActivity.kt
+++ playgroundForAndroid/app/src/main/java/com/gogumac/playgroundforandroid/MainActivity.kt
@@ -6,6 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gogumac.playgroundforandroid.databinding.ActivityMainBinding
+import kotlinx.serialization.Serializable
+
+@Serializable
+object ScreenA
+
+@Serializable
+data class ScreenB(val arg: String)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +25,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
