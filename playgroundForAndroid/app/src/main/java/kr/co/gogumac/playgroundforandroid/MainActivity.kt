fa686ad087450d47c31d2b922e93a2d67da36a48
gogumaC(mac mini) <kimu2371@naver.com>
2024-08-15 01:12:42 +0900

    0. Set composable

--- playgroundForAndroid/app/src/main/java/kr/co/gogumac/playgroundforandroid/MainActivity.kt
+++ playgroundForAndroid/app/src/main/java/kr/co/gogumac/playgroundforandroid/MainActivity.kt
@@ -3,13 +3,18 @@ package kr.co.gogumac.playgroundforandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.co.gogumac.playgroundforandroid.ui.theme.PlaygroundForAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,12 +27,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ScreenA()
                 }
             }
         }
     }
 }
+
+@Composable
+fun ScreenA(modifier: Modifier=Modifier){
+    Box(modifier = modifier
+        .padding(10.dp)
+        .background(color = Color.Green)){
+        ScreenB(Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun ScreenB(modifier: Modifier=Modifier){
+    Box(modifier = modifier
+        .padding(10.dp)
+        .background(color = Color.Yellow)){
+        ScreenC(Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun ScreenC(modifier: Modifier=Modifier){
+    Box(modifier = modifier
+        .padding(10.dp)
+        .background(color = Color.Blue)){
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
