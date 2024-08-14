2f39edf8d9c0c7ae33d4a85c9041bb0c08082d0f
gogumaC(mac mini) <kimu2371@naver.com>
2024-08-15 01:16:52 +0900

    1. Set BackHandler

--- playgroundForAndroid/app/src/main/java/kr/co/gogumac/playgroundforandroid/MainActivity.kt
+++ playgroundForAndroid/app/src/main/java/kr/co/gogumac/playgroundforandroid/MainActivity.kt
@@ -1,7 +1,9 @@
 package kr.co.gogumac.playgroundforandroid
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.co.gogumac.playgroundforandroid.ui.theme.PlaygroundForAndroidTheme
@@ -36,6 +39,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ScreenA(modifier: Modifier=Modifier){
+    val context= LocalContext.current
+    BackHandler {
+        Toast.makeText(context,"ScreenA",Toast.LENGTH_SHORT).show()
+    }
     Box(modifier = modifier
         .padding(10.dp)
         .background(color = Color.Green)){
@@ -45,6 +52,10 @@ fun ScreenA(modifier: Modifier=Modifier){
 
 @Composable
 fun ScreenB(modifier: Modifier=Modifier){
+    val context= LocalContext.current
+    BackHandler {
+        Toast.makeText(context,"ScreenB",Toast.LENGTH_SHORT).show()
+    }
     Box(modifier = modifier
         .padding(10.dp)
         .background(color = Color.Yellow)){
@@ -54,6 +65,10 @@ fun ScreenB(modifier: Modifier=Modifier){
 
 @Composable
 fun ScreenC(modifier: Modifier=Modifier){
+    val context= LocalContext.current
+    BackHandler {
+        Toast.makeText(context,"ScreenC",Toast.LENGTH_SHORT).show()
+    }
     Box(modifier = modifier
         .padding(10.dp)
         .background(color = Color.Blue)){
